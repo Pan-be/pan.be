9d8688ace5e462137da1e5729722a05d7cc077de
Konrad <konradbieniek@gmail.com>
2023-02-12 22:43:51 +0100

    added getStaticProps and getStaticPaths to single project page

---
+++ src/pages/[projectId]/index.js
@@ -0,0 +1,46 @@
+import ProjectDetail from "@/components/projects/ProjectDetail"
+
+const DetailPage = () => {
+	return (
+		<ProjectDetail
+			img='https://earlystemer.com/wp-content/uploads/2021/09/html-structure-1024x588.png'
+			title='First Project'
+			stack='Name of stack'
+			description='This project is a modern and dynamic web application, built using the latest technologies in JavaScript development. It features a clean and user-friendly interface, with a focus on efficient navigation and seamless user experience. The project utilizes cutting-edge libraries such as React, Redux, and Webpack, to deliver a fast and responsive website that can handle complex data structures with ease. With its modular architecture and scalable design, this project is the perfect solution for businesses and organizations looking to take their online presence to the next level. Whether you need to showcase products, manage customer information, or deliver dynamic content, this project has everything you need to succeed in the digital world.'
+		/>
+	)
+}
+
+export const getStaticPaths = () => {
+	return {
+		fallback: false,
+		paths: [
+			{
+				params: {
+					projectId: "p1",
+				},
+			},
+		],
+	}
+}
+
+export const getStaticProps = async (context) => {
+	const projectId = context.params.projectId
+
+	console.log(projectId)
+
+	return {
+		props: {
+			projectData: {
+				id: "p1",
+				img: "https://earlystemer.com/wp-content/uploads/2021/09/html-structure-1024x588.png",
+				title: "First Project",
+				stack: "Name of stack",
+				description:
+					"This project is a modern and dynamic web application, built using the latest technologies in JavaScript development. It features a clean and user-friendly interface, with a focus on efficient navigation and seamless user experience. The project utilizes cutting-edge libraries such as React, Redux, and Webpack, to deliver a fast and responsive website that can handle complex data structures with ease. With its modular architecture and scalable design, this project is the perfect solution for businesses and organizations looking to take their online presence to the next level. Whether you need to showcase products, manage customer information, or deliver dynamic content, this project has everything you need to succeed in the digital world.",
+			},
+		},
+	}
+}
+
+export default DetailPage
